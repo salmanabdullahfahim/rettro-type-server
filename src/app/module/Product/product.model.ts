3ceae1b08b124df9916a8546533b309785e276de
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-07-09 12:47:01 +0600

    Product schema created

---
+++ src/app/module/Product/product.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  availableQuantity: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Product = model<TProduct>("Product", productSchema);
